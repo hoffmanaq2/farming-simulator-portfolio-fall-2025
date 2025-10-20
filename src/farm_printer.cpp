2040e4470fe6385ec6e87bb5f4b79c72974531da
ha63211 <ha63211@mail.harpercollege.edu>
2025-10-19 20:48:27 -0500

    Implemented legend & day counter

--- src/farm_printer.cpp
+++ src/farm_printer.cpp
@@ -1,16 +1,17 @@
+#include "farm_printer.hpp"
+#include "farm.hpp"
 #include <string>
 
-#include "farm.hpp"
-#include "farm_printer.hpp"
-
-FarmPrinter::FarmPrinter(Farm *farm) : farm(farm) {}
+FarmPrinter::FarmPrinter(Farm* farm) : farm(farm) {}
 
 std::string FarmPrinter::pp() {
-    std::string output = "";
-    for(int i = 0; i < farm->number_of_rows(); i++) {
-        for(int j = 0; j < farm->number_of_columns(); j++) {
-            output += farm-> get_symbol(i, j);
-            output += " ";
+    std::string output = "Day " + std::to_string(farm->day()) + "\n";
+    output += "Legend: @=Player  .=Soil  v=Baby Carrot  V=Mature Carrot\n";
+    output += "Controls: w=up, s=down, a=left, d=right, c=plant, h=harvest, e=end day, q=quit\n\n";
+
+    for (int i = 0; i < farm->number_of_rows(); i++) {
+        for (int j = 0; j < farm->number_of_columns(); j++) {
+            output += farm->get_symbol(i, j) + " ";
         }
         output += "\n";
     }
--- src/farm_printer.hpp
+++ src/farm_printer.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <string>
+#include "farm.hpp"
 
 class FarmPrinter {
 private:
