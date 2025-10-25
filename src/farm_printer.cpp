6fe16d439c610c60123f68abe66651b75c7d454d
ha63211 <ha63211@mail.harpercollege.edu>
2025-10-25 11:51:17 -0500

    pp layout change

--- src/farm_printer.cpp
+++ src/farm_printer.cpp
@@ -5,9 +5,10 @@
 FarmPrinter::FarmPrinter(Farm* farm) : farm(farm) {}
 
 std::string FarmPrinter::pp() {
-    std::string output = "Day " + std::to_string(farm->day()) + "\n";
-    output += "Legend: @=Player  .=Soil  v=Baby Carrot  V=Mature Carrot\n";
-    output += "Controls: w=up, s=down, a=left, d=right, c=plant, h=harvest, e=end day, q=quit\n\n";
+
+    std::string output = "Legend:      @=Player     .=Soil      v=Baby Carrot       V=Mature Carrot\n\n";
+    output += "Controls:    w=up   s=down   a=left   d=right   c=plant   h=harvest   e=end day   q=quit\n\n"; //3 spaces
+    output += "Day " + std::to_string(farm->day()) + "\n";
 
     for (int i = 0; i < farm->number_of_rows(); i++) {
         for (int j = 0; j < farm->number_of_columns(); j++) {
