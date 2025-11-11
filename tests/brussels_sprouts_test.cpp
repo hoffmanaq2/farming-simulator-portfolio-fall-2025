9c18fbdae58b1b54b3da7a628ce02156a8bbb6c2
ha63211 <ha63211@mail.harpercollege.edu>
2025-11-11 16:18:29 -0600

    tests (filler)

---
+++ tests/brussels_sprouts_test.cpp
@@ -0,0 +1,13 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/benchmark/catch_constructor.hpp>
+#include <catch2/generators/catch_generators_range.hpp>
+
+#include "../src/brussels_sprouts.hpp"
+
+//Filer
+TEST_CASE ("Carrot returns a v when it is a seedling") {
+    Carrot carrot;
+    REQUIRE( carrot.symbol() == "v" );
+    REQUIRE_FALSE(carrot.is_mature());
+}
