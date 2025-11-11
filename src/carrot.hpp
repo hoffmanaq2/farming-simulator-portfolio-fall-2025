ef149be0c9d08e9f84f6c833df01b37bd89e13ca
ha63211 <ha63211@mail.harpercollege.edu>
2025-11-11 16:16:50 -0600

    Add water() & realistic plant behavior

--- src/carrot.hpp
+++ src/carrot.hpp
@@ -6,10 +6,14 @@
 
 class Carrot : public Plot {
 private:
-    bool mature = false;
+    int days_planted = 0; // 0 = just planted (tilled soil)
+    bool watered_today = false;
+    static const int days_to_sprout = 1;
+    static const int days_after_sprout_to_mature = 1; // additional days
 
 public:
     std::string symbol() override;
     void end_day() override;
-    bool is_mature() const;
+    bool is_mature() const override;
+    void water() override;
 };
