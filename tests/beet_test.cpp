da62d3c21421ea7937c8bb2e94104214da7f07ce
ha63211 <ha63211@mail.harpercollege.edu>
2025-11-11 22:28:59 -0600

    Beet tests

--- tests/beet_test.cpp
+++ tests/beet_test.cpp
@@ -5,9 +5,57 @@
 
 #include "../src/beet.hpp"
 
-//Filer
-TEST_CASE ("Carrot returns a v when it is a seedling") {
-    Carrot carrot;
-    REQUIRE( carrot.symbol() == "v" );
-    REQUIRE_FALSE(carrot.is_mature());
+TEST_CASE("Beet starts as seed when planted") {
+    Beet beet;
+    REQUIRE(beet.symbol() == "x");
+    REQUIRE_FALSE(beet.is_mature());
+}
+
+TEST_CASE("Beet growth over days without watering & stays mature after maturity") {
+    Beet beet;
+
+    beet.end_day(); // Day 1
+    REQUIRE(beet.symbol() == "x");
+    REQUIRE_FALSE(beet.is_mature());
+
+    beet.end_day(); // Day 2 - becomes "u"
+    REQUIRE(beet.symbol() == "u");
+    REQUIRE_FALSE(beet.is_mature());
+
+    for (int i = 0; i < 5; ++i) beet.end_day(); //+5 days (7 days overall)
+    REQUIRE(beet.symbol() == "U");
+    REQUIRE(beet.is_mature());
+
+    beet.end_day(); // Extra day
+    REQUIRE(beet.symbol() == "U");
+    REQUIRE(beet.is_mature());
+}
+
+TEST_CASE("Beet matures faster when watered") {
+    Beet beet;
+    beet.water();
+    beet.end_day(); // +2 days
+    REQUIRE(beet.symbol() == "u");
+    REQUIRE_FALSE(beet.is_mature());
+
+    beet.end_day(); // +1 day (3 total)
+    REQUIRE(beet.symbol() == "u");
+    REQUIRE_FALSE(beet.is_mature());
+
+    beet.water();
+    beet.end_day(); // +2 more days (5 total)
+    REQUIRE(beet.symbol() == "u");
+    REQUIRE_FALSE(beet.is_mature());
+
+    beet.water();
+    beet.end_day(); // +2 more days (7 total)
+    REQUIRE(beet.symbol() == "U");
+    REQUIRE(beet.is_mature());
+}
+
+TEST_CASE("Beet stays mature after reaching maturity") {
+    Beet beet;
+    for (int i = 0; i < 8; ++i) beet.end_day(); // 8 days overall
+    REQUIRE(beet.symbol() == "U");
+    REQUIRE(beet.is_mature());
 }
